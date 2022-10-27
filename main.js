a3c91267defb189d67c2db6e1e49004fac576749
Javier <alejo982014@outlook.com>
2022-10-26 22:51:50 -0500

    Desafio api finalizado

---
+++ main.js
@@ -0,0 +1,28 @@
+let $container = document.getElementById("container");
+let fragmento = document.createDocumentFragment();
+let url = "https://larnu-dev-upy5mhs63a-rj.a.run.app/api/v1/categories";
+
+function axiosLarnU(){
+  axios
+  .get(url)
+  .then((res) => {
+    let json = res.data.communityCategories;
+    console.log(json);
+    json.forEach((e)=>{
+      $container.innerHTML += `
+      <div class="container__card">
+          <div class="container__icon"><img src="${e.icon}" class="img"></div>
+          <h2 class="title">${e.name}</h2>
+          <p class="totalQ">Total Quizzes: ${e.totalQuizzes}</p>
+          <P class="user">Users: ${e.users}</P>
+          <a href="https://es.larnu.com/" class="link btn" target="_blank">Go to LarnU</a>
+        </div>
+      ` 
+    })
+  }).catch((err) => {
+    let mensaje = err.statusText || 'Ocurrio un error'
+      $lista.innerHTML = `Error ${err.status}: ${mensaje}`
+  });
+}
+axiosLarnU();
+
